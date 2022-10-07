0c2112c2ced2c6ddc7031825bdc532ea060fddf3
flnx <32278279+flnx@users.noreply.github.com>
2022-10-07 21:13:37 +0300

    updating exercises

--- 3. Arrays/Exercises/5.maxNumber.js
+++ 3. Arrays/Exercises/5.maxNumber.js
@@ -1,23 +1,27 @@
-function solve(array) {
-  let newArray = [];
-  let arrayL = array.length;
+// function maxNumber(array) {
+//   let newArray = [];
+//   let arrayL = array.length;
 
-  for (let i = 0; i < arrayL; i++) {
-    let isTheBiggest = true;
+//   for (let i = 0; i < arrayL; i++) {
+//     let isTheBiggest = true;
 
-    for (let r = i + 1; r < arrayL; r++) {
-      if (array[i] <= array[r]) {
-        isTheBiggest = false;
-      }
-    }
+//     for (let r = i + 1; r < arrayL; r++) {
+//       if (array[i] <= array[r]) {
+//         isTheBiggest = false;
+//       }
+//     }
 
-    if (isTheBiggest) {
-      newArray.push(array[i]);
-    }
-  }
+//     if (isTheBiggest) {
+//       newArray.push(array[i]);
+//     }
+//   }
 
-  console.log(newArray.join(' '));
-}
+//   console.log(newArray.join(' '));
+// }
 
-solve([41, 41, 34, 20]);
-solve([14, 24, 3, 19, 15, 17]);
+maxNumber = (arr) => arr.filter((x, i) => arr.slice(i + 1).every(y => x > y)).join(' ');
+
+console.log(maxNumber([41, 41, 34, 20]));
+
+maxNumber([41, 41, 34, 20]);
+maxNumber([14, 24, 3, 19, 15, 17]);
--- 4. Functions/Lab/5.simpleCalculator.js
+++ 4. Functions/Lab/5.simpleCalculator.js
@@ -19,25 +19,21 @@
 //     console.log(res);
 // }
 
-function solve(numOne, numTwo, operator) {
-  let res;
+// Updated Solution
 
-  switch (operator) {
-    case 'multiply':
-      res = (a, b) => a * b;
-      break;
-    case 'divide':
-      res = (a, b) => a / b;
-      break;
-    case 'add':
-      res = (a, b) => a + b;
-      break;
-    case 'subtract':
-      res = (a, b) => a - b;
-      break;
+function solve(numOne, numTwo, operator) {
+  const calculator = {
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
   }
 
-  console.log(res(numOne, numTwo));
+  const result = calculator[operator]
+  console.log(result(numOne, numTwo));
 }
 
 solve(5, 5, 'add');
+
+
+
