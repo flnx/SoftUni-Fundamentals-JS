f1404793732e94cb0bfb39629a3f076174ee2e69
flnx <32278279+flnx@users.noreply.github.com>
2022-10-19 11:13:26 +0300

    updating

--- 3. Arrays/Lab/7.equalArrays.js
+++ 3. Arrays/Lab/7.equalArrays.js
@@ -1,6 +1,6 @@
 function equalArrays(arrOne, arrTwo) {
   for (let i = 0; i < arrOne.length; i++) {
-    if (+arrOne[i] != +arrTwo[i]) {
+    if (Number(arrOne[i]) != Number(arrTwo[i])) {
       return `Arrays are not identical. Found difference at ${i} index`;
     }
   }
@@ -8,43 +8,41 @@ function equalArrays(arrOne, arrTwo) {
   const sum = arrOne.map(Number).reduce((acc, x) => acc + x, 0);
   return `Arrays are identical. Sum: ${sum}`;
 }
-console.log(equalArrays(['10', '20', '30'], ['10', '20', '30'])); 
-console.log(equalArrays(['1', '2', '3', '4', '5'], ['1', '2', '4', '4', '5'])); 
-console.log(equalArrays(['1'], ['10'])); 
+console.log(equalArrays(['10', '20', '30'], ['10', '20', '30']));
+console.log(equalArrays(['1', '2', '3', '4', '5'], ['1', '2', '4', '4', '5']));
+console.log(equalArrays(['1'], ['10']));
 
 // Arrays are identical. Sum: 60
 // Arrays are not identical. Found difference at 2 index
 // Arrays are not identical. Found difference at 0 index
 
- // Old Solution
 
-// function solve(arr1, arr2) {
-//   let isEqual = true;
-//   let sum = 0;
 
-//   for (let i = 0; i < arr1.length; i++) {
-//     arr1[i] = Number(arr1[i]);
-//   }
+console.log(`------------------------------------------`);
+// Old Solution
 
-//   for (let j = 0; j < arr2.length; j++) {
-//     arr2[j] = Number(arr2[j]);
-//   }
+function solve(arr1, arr2) {
+  let isEqual = true;
+  let sum = 0;
 
-//   for (let k = 0; k < arr1.length; k++) {
-//     if (arr1[k] !== arr2[k]) {
-//       isEqual = false;
-//       console.log(`Arrays are not identical. Found difference at ${k} index`);
-//       break;
-//     } else {
-//       sum += arr1[k];
-//     }
-//   }
+  arr1 = arr1.map(Number);
+  arr2 = arr2.map(Number);
 
-//   if (isEqual) {
-//     console.log(`Arrays are identical. Sum: ${sum}`);
-//   }
-// }
+  for (let k = 0; k < arr1.length; k++) {
+    if (arr1[k] !== arr2[k]) {
+      isEqual = false;
+      console.log(`Arrays are not identical. Found difference at ${k} index`);
+      break;
+    } else {
+      sum += arr1[k];
+    }
+  }
 
-// solve(['10', '20', '30'], ['10', '20', '30']);
-// solve(['1', '2', '3', '4', '5'], ['1', '2', '4', '4', '5']);
-// solve(['1'], ['10']);
+  if (isEqual) {
+    console.log(`Arrays are identical. Sum: ${sum}`);
+  }
+}
+
+solve(['10', '20', '30'], ['10', '20', '30']);
+solve(['1', '2', '3', '4', '5'], ['1', '2', '4', '4', '5']);
+solve(['1'], ['10']);
